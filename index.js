b375f592326d423be47f9d0ea69f975961f6ddf6
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-06-06 13:57:22 +0900

    delete

--- index.js
+++
@@ -1,9 +0,0 @@
-const { createElement } = require("react");
-
-const title = document.querySelector(`h2`);
-title.innerText = "안녕하세요!";
-title.style.color = `blue`;
-
-const title1 = document.createElement('h1');
-title.innerText = "새로운 친구에여"
-title.style.color = `red`
