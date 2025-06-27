f84d5660a1ef7212c18a8e43826d1b6543e8f5ac
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-06-27 15:58:46 +0900

    2025-06-27 vue

--- practice/index.js
+++ practice/index.js
@@ -1,9 +1,11 @@
-const { createElement } = require("react");
-
 const title = document.querySelector(`h2`);
 title.innerText = "안녕하세요!";
 title.style.color = `blue`;
 
+const body = document.querySelector(`body`)
+
 const title1 = document.createElement('h1');
 title.innerText = "새로운 친구에여"
 title.style.color = `red`
+
+body.appendChild(title)
