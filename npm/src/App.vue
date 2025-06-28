fee15ba3758b7de847e34af635b38350f9cf823f
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-06-28 20:19:25 +0900

    2025-06-28

---
+++ npm/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  //sfc에서 사용되는 로직을 자바스크립트로 작성하는 영역s
+</script>
+
+<template>
+  <h1>Hello , SFC</h1>
+  <p>SFC는 Single File Compoenet의 약자입니다 </p>
+</template>
+
+<style>
+h1 , p {
+  color: green;
+}
+</style>
