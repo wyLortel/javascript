996298c662586cb8dd9e2edc6905608582ed47dc
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-06-30 22:16:59 +0900

    2025-06-30

--- ex3/src/App.vue
+++ ex3/src/App.vue
@@ -1,4 +1,178 @@
-<script>
+`<script>
+
+export default{
+  data() {
+    return {
+      field: "",
+      files: null,
+    };
+  },
+  methods: {
+    printData() {
+      console.log(this.files);
+    },
+    onChangeHandler(e) {
+      console.log(e);
+      this.files = e.target.files[0];
+    }
+  },
+};
+</script>
+
+<template>
+  <input type="file" @change=" $event => onChangeHandler($event)">
+  <button @click="printData">업로드</button>
+</template>
+`
+
+
+
+
+
+
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      filed : 67,
+    };
+  },
+  methods : {
+    printData() {
+      console.log(this.filed)
+    },
+  },
+};
+</script>
+
+<template>
+  <progress max="100" v-bind:value="filed" value="50">{{ filed }}%</progress> <br>
+  <button @click="printData">출력</button>
+</template> -->
+
+
+
+
+
+<!-- <script>
+export default {
+  data() {
+    return{
+      field : "female",
+    };
+  },
+  methods: {
+    printData() {
+      console.log(this.field);
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="radio" value="male" name="gender" v-model="field">male<br>
+  <input type="radio" value="female" name="gender" v-model="field">female<br>
+  <button @click="printData">출력</button>
+</template> -->
+
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      filed: ["banana"],  
+    };
+  },
+  methods: {
+    printData() {
+      console.log(this.filed);  
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="checkbox" v-model="filed" value="banana">banana<br/>
+  <input type="checkbox" v-model="filed" value="orange">orange<br/>
+  <button @click="printData">출력</button>
+</template>
+
+ -->
+
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      field: "init",
+      combo: "female",
+    };
+  },
+  methods : {
+  printData() {
+    // console.log(this.field);
+    console.log(this.combo)
+    },
+  },
+};
+</script>
+
+<template>
+  <textarea v-model="field"></textarea>
+  <select v-model="combo">
+    <option value="male">male</option>
+    <option value="female">female</option>
+  </select>
+  <button @click="printData">출력</button>
+</template>
+ -->
+
+
+
+
+
+<!-- <script>
+export default {
+  data(){
+    return {
+      str : "",
+    };
+  },
+  methods : {
+    onInputHandler(e) {
+      this.str = e.target.value
+    }
+  }
+};
+</script>
+
+<template>
+  <textarea @input="onInputHandler($event)"></textarea>
+  <p>글자수 : {{ str.length }}</p>
+</template> -->
+
+
+
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      id : "sucoding"
+    };
+  },
+};
+</script>
+
+<template>
+  <input type="text" name="uid" v-model="id">
+  {{ id }}
+</template> -->
+
+
+
+<!-- <script>
 export default {
   data() {
     return {
@@ -19,8 +193,14 @@ export default {
 
 <template>
   <form >
-    <input type="text" name="uid" @change="id = $event.target.value " v-bind:value="id">
-    <input type="password" name="pw" @change="pw = $event.target.value" v-bind:value="pw">
+    <input
+      type="text" 
+      name="uid" 
+      v-model="id">
+    <input 
+      type="password"
+      name="pw"
+      v-model="pw">
     <button type="button" @click="login">로그인</button>
   </form>
-</template>
+</template> -->
---
+++ ex4/src/App.vue
@@ -0,0 +1,135 @@
+<script>
+export default {
+  methods: {
+    onkeydownHandler(event) {
+      console.log(event)
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="text" @keydown="onkeydownHandler">
+</template>
+
+
+
+
+
+<!-- <script>
+export default {
+  methods: {
+    onClickHandler() {
+      console.log("click");
+    },
+  },
+};
+</script>
+
+
+
+<template>
+  <button @click.once="onClickHandler">클릭</button>
+</template>
+ -->
+
+
+
+
+
+<!-- <script>
+export default{
+  methods: {
+    handleScrollPassive(event){
+      // event.preventDefault();
+      console.log("wheel")
+    },
+  },
+};
+</script>
+
+
+
+<template>
+  <div class="parent" @wheel="handleScrollPassive">
+    <div class="child">wheel</div>
+  </div>
+</template>
+
+
+<style>
+.parent {
+  height: 200px;
+  overflow-y: scroll;
+  border: 3px solid red;
+}
+
+.child {
+  height: 2000px;
+}
+</style> -->
+
+
+
+
+
+<!-- <script>
+export default {
+  methods: {
+    parent() {
+      console.log("parent");
+    },
+    child() {
+      console.log("child");
+    },
+  },
+};
+</script>
+
+
+<template>
+  <div class="parent" @click.self="parent">
+  <div class="child" @click.self="child"></div>
+  </div>
+</template>
+
+<style>
+.parent {
+  padding: 20px;
+  background-color: red;
+}
+
+.child {
+  padding: 20px;
+  background-color: blue;
+}
+</style>
+ -->
+
+
+
+
+<!-- <script>
+export default {
+  methods: {
+   onSubmitHandle() {
+    console.log("start")
+   },
+  },
+};
+</script>
+
+
+<template>
+  <div>
+    <a href="https://www.google.com/?hl=ko" @click.prevent>구글</a>
+    <form @submit.prevent>
+      <input type="text" placeholder="onSubmitHandle" />
+      <button>제출</button>
+    </form>
+  </div>
+</template>
+
+
+<style>
+</style> -->
